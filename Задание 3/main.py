5e08c56771caa254bec05a1e31d60b9f8d76152b
13-arch <Iron_Mike@tutamail.com>
2026-06-08 10:49:31 +0300

    Add files via upload

--- Задание 3/main.py
+++ Задание 3/main.py
@@ -1,8 +1,9 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import Column, Date, Integer, String, Time, create_engine, text
+from sqlalchemy.dialects.mssql import UNIQUEIDENTIFIER
 from sqlalchemy.orm import DeclarativeBase
 
-database_url_1 = r"mssql+pyodbc://sa:Senya_13.05.07@15-2-441-17\HICO69/Sector?driver=ODBC+Driver+17+for+SQL+Server&Encrypt=yes&TrustServerCertificate=yes&PersistSecurityInfo=no&Pooling=no&MultipleActiveResultSets=no"
+database_url_1 = r"mssql+pyodbc://sa:Senya_13.05.07@15-2-441-17\HICO69/User_Actions?driver=ODBC+Driver+17+for+SQL+Server&Encrypt=yes&TrustServerCertificate=yes&PersistSecurityInfo=no&Pooling=no&MultipleActiveResultSets=no"
 
 engine1 = create_engine(database_url_1)
 engine2 = create_engine(database_url_1)
@@ -23,10 +24,9 @@ engine_list = [engine1 ,engine2,engine3, engine4, engine5, engine6 ,engine7 ,eng
 class Base(DeclarativeBase):
     pass
 
-class Logs(Base):
+class User_Logs(Base):
     __tablename__ = "User_Logs"
-
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(UNIQUEIDENTIFIER, primary_key=True, default=text("NEWID()"))
     username = Column(String, nullable=False)
     user_action = Column(String, nullable=False)
     action_date = Column(Date, nullable=False)
@@ -35,22 +35,32 @@ class Logs(Base):
 
 Base.metadata.create_all(bind=engine1)
 Base.metadata.create_all(bind=engine2)
+Base.metadata.create_all(bind=engine3)
+Base.metadata.create_all(bind=engine4)
+Base.metadata.create_all(bind=engine5)
+Base.metadata.create_all(bind=engine6)
+Base.metadata.create_all(bind=engine7)
+Base.metadata.create_all(bind=engine8)
+Base.metadata.create_all(bind=engine9)
+Base.metadata.create_all(bind=engine10)
+Base.metadata.create_all(bind=engine11)
+Base.metadata.create_all(bind=engine12)
 
 
-def inputt(engine:str, input_data:Logs):
+def inputt(engine:str, input_data:User_Logs):
     with Session(autoflush=False, bind=engine) as db:
         db.add(input_data)
         db.commit()
 
-def sendto_shard(input_data:Logs, memory):
+def sendto_shard(input_data:User_Logs, memory):
     inputt(engine=engine_list[memory], input_data = input_data)
     print(f"bd {memory+1}")
-    memory = (memory + 1) % 10
+    memory = (memory + 1) % 12
     return memory
 
 
 def main():
-    input_data = Logs(username = "asd", user_action = "DELETE", action_date = "2023-01-01", action_time = "00:00:00", action_result = "OK")
+    input_data = User_Logs(username = "sen", user_action = "DELETE", action_date = "2025-01-01", action_time = "00:00:00", action_result = "OK")
     memory = 0
     memory = sendto_shard(input_data, memory=memory)
     memory = sendto_shard(input_data, memory=memory)
@@ -64,6 +74,7 @@ def main():
     memory = sendto_shard(input_data, memory=memory)
     memory = sendto_shard(input_data, memory=memory)
     memory = sendto_shard(input_data, memory=memory)
+    memory = sendto_shard(input_data, memory=memory)    
 
 
 if __name__ == "__main__":
